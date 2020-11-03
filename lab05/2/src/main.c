4404ed66149cb2201f53d2cf8e95487dfa2c97e8
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-03 23:02:23 +0200

    просте число

--- lab05/2/src/main.c
+++ lab05/2/src/main.c
@@ -11,5 +11,31 @@ int main () {
 			result = 0;//число складене
 		}
 	}
+	int temp_while = 1;
+	int result_while = 0;
+	int numberOfDivisors_while = 0;
+	while (temp_while <= SIMPLENUM) {
+		numberOfDivisors_while = SIMPLENUM % temp_while;
+		if (numberOfDivisors_while == 0 && numberOfDivisors_while <= 2) {
+			result_while = 1;//число просте
+		}
+		else { 
+			result_while = 0;//число складене
+		}
+		temp_while++;
+	}
+	int temp_do = 0;
+	int result_do = 0;
+	int numberOfDivisors_do = 0;
+	while (temp_do <= SIMPLENUM) {
+		numberOfDivisors_do = SIMPLENUM % temp_do;
+		if (numberOfDivisors_do == 0 && numberOfDivisors_do <= 2) {
+			result_do = 1;
+		}
+		else {
+			result_do = 0;
+		}
+		temp_do++;
+	}
 return 0;
 }
