792dda8a82febbafb319f9ae3433facb88e17b73
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-04 21:41:34 +0200

    найбільший спільний дільник

--- lab05/1/src/main.c
+++ lab05/1/src/main.c
@@ -1,28 +1,36 @@
-int main () {
-	int NUM1 = 16;// задане число
-	int NUM2 = 20;// задане число
+#define X 16// задане число
+#define Y 20// задане число
+int main () { 
+	int num1;
+	int num2;
+	int nod_for = 0;
 	int nod_while = 0;
-	while (NUM1 != 0 && NUM2 != 0) { /*ділимо більше число на менше доки остача від ділення не буде 0*/  
-		if (NUM1 > NUM2) {	
-			NUM1 %= NUM2;//nod_while = NUM1 % NUM2;
-			//NUM1 = NUM2;
-			//NUM2 = nod_while;
-		} else if (NUM1 < NUM2) {
-			NUM2 %= NUM1;//nod_while = NUM2 % NUM1;
-			//NUM2 = NUM1;
-			//NUM1 = nod_while;
+	int nod_do = 0;
+	for (num1 = X, num2 = Y; num1 > 0 && num2 > 0;) {/*ділимо більше число на менше доки остача від ділення не буде 0 (алгоритм Евкліда)*/  
+
+		(num1 > num2) ? (num1 %= num2) : (num2 %= num1);
+		nod_for = num1 + num2;
+	}
+	num1 = X;//щоб увійти у новий цикл, минулі значення змінних необхідно "скинути"
+	num2 = Y;
+	while (num1 > 0 && num2 > 0) { /*ділимо більше число на менше доки остача від ділення не буде 0*/  
+		if (num1 > num2) {	
+			num1 %= num2;
+		} else if (num1 < num2) {
+			num2 %= num1;
+		nod_while = num1 + num2;
 		}
 	}
-	do { 
-		if (NUM1 > NUM2) {      
-			NUM1 %= NUM2;//nod_while = NUM1 % NUM2;
-                        //NUM1 = NUM2;
-                        //NUM2 = nod_while;
-		} else if (NUM1 < NUM2) {
-			NUM2 %= NUM1;//nod_while = NUM2 % NUM1;
-                        //NUM2 = NUM1;
-                        //NUM1 = nod_while;
+	num1 = X;
+	num2 = Y;
+	do { /*ділимо більше число на менше доки остача від ділення не буде 0*/  
+
+		if (num1 > num2) {      
+			num1 %= num2;
+		} else if (num1 < num2) {
+			num2 %= num1;
 		}      
-	} while (NUM1 != 0 && NUM2 != 0);
+		nod_do = num1 + num2;
+	} while (num1 > 0 && num2 > 0);
 return 0;
 }	
