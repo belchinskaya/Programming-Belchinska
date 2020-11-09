d162e5b34ff073c3a1993ea368ee20f91d00b172
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-09 12:03:21 +0200

    просте число

--- lab05/2/src/main.c
+++ lab05/2/src/main.c
@@ -1,41 +1,38 @@
-#define SIMPLENUM 3//задане число
+#define NUM 3//задане число
 int main () {
-	int numberOfDivisors  = 0;
-	int result = 0;
-	for (int i = 1; i <= SIMPLENUM; i++) {/*якщо число ділиться націло (тобто остача від ділення =0) тільки на 1 та на саме себе, то воно просте*/
-		numberOfDivisors = SIMPLENUM % i;
-		if (numberOfDivisors == 0 && numberOfDivisors <= 2) {
-			result = 1;//число просте
-		}
-		else { 
-			result = 0;//число складене
+	int tempFor  = 0;
+	int resultFor = 0;
+	for (int i = 2; i < NUM; i++) {/*якщо число ділиться націло (тобто остача від ділення =0) на число менше за нього, то воно складене*/
+		tempFor = NUM % i;
+		if (tempFor == 0) {
+			resultFor = 0;//число складене
+		} else { 
+			resultFor = 1;//число просте
 		}
 	}
-	int temp_while = 1;
-	int result_while = 0;
-	int numberOfDivisors_while = 0;
-	while (temp_while <= SIMPLENUM) {
-		numberOfDivisors_while = SIMPLENUM % temp_while;
-		if (numberOfDivisors_while == 0 && numberOfDivisors_while <= 2) {
-			result_while = 1;//число просте
+	int i = 2;
+	int resultWhile = 0;
+	int tempWhile = 0;
+	while (i < NUM) {
+		tempWhile = NUM % i;
+		if (tempWhile == 0) {
+			resultWhile = 0;//число складене
+		} else { 
+			resultWhile = 1;//число просте
 		}
-		else { 
-			result_while = 0;//число складене
-		}
-		temp_while++;
+		i++;
 	}
-	int temp_do = 0;
-	int result_do = 0;
-	int numberOfDivisors_do = 0;
-	while (temp_do <= SIMPLENUM) {
-		numberOfDivisors_do = SIMPLENUM % temp_do;
-		if (numberOfDivisors_do == 0 && numberOfDivisors_do <= 2) {
-			result_do = 1;
+	int j = 2;
+	int resultDo = 0;
+	int tempDo = 0;
+	do  {
+		tempDo = NUM % j;
+		if (tempDo == 0) {
+			resultDo = 0;//число складене
+		} else {
+			resultDo = 1;//число просте
 		}
-		else {
-			result_do = 0;
-		}
-		temp_do++;
-	}
-return 0;
+		j++;
+	} while (j < NUM);
+	return 0;
 }
