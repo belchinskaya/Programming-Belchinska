5f80ee9d30b2a9878217913c7a1cc911dd9e7590
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-09 09:24:36 +0200

    змістити числа в строці двумірного масиву на 1

---
+++ lab06/5/src/main.c
@@ -0,0 +1,17 @@
+#define N 3
+int main() {
+	int arrayIn[N][N];
+	for (int i = 0; i < N; i++) {//перебираємо елементи масиву
+		for (int j = 0;j < N; j++) {
+			arrayIn[i][j] = i * N + j;
+		}
+	}
+	for (int i = 0; i < N; i++) {//за допомгою додаткової змінної k перебираємо строки заданої матриці
+		int k = arrayIn[i][0];
+		for (int j = 0; j < N; j++) {//зміщуємо елементи масиву на 1 вправо
+			arrayIn[i][j] = arrayIn[i][j + 1]; 
+		}
+		arrayIn[i][N - 1] = k;//результат прирівнюємо знов до масиву
+	}
+	return 0;
+}
