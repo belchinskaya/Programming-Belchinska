f9b90ae4357f987bffe780e0861fb8a3d974dba4
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-15 13:03:38 +0200

    обчислити y

---
+++ lab04/5/src/main.c
@@ -0,0 +1,17 @@
+#define X -2
+int main() {
+	int y = 0;
+	if (X <= -1 && X >= -3) {
+		y = -1 * X - 1;
+	}
+	else if (X >= -1 && X <= 0) {
+		y = X + 1;
+	}
+	else if (X >= 0 && X <= 1) {
+		y = -1 * X + 1;
+	}
+	else if (X >=1 && X <= 3) {
+		y = X -1;
+	}
+	return 0;
+}
