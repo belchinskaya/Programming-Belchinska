3d9aceadcbd48ead0cf6225ff2034b38029f55c6
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-16 19:44:52 +0200

    графік y(x)

--- lab04/5/src/main.c
+++ lab04/5/src/main.c
@@ -1,17 +1,20 @@
 #define X -2
 int main() {
 	int y = 0;
-	if (X <= -1 && X >= -3) {
+	if (X < -1) {
 		y = -1 * X - 1;
 	}
-	else if (X >= -1 && X <= 0) {
-		y = X + 1;
+	else if (X == -1) {
+		y = 0;
 	}
-	else if (X >= 0 && X <= 1) {
-		y = -1 * X + 1;
+	else if (X != 0) {
+		y = 1;
 	}
-	else if (X >=1 && X <= 3) {
-		y = X -1;
+	else if (X == 1) {
+		y = 0;
+	}
+	else if (X > 1) {
+		y = X - 1;
 	}
 	return 0;
 }
