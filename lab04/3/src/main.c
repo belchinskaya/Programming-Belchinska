2a301c2e8f11f5bedfb1f766c9d56d90656e802b
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-16 17:50:34 +0200

    порівняння чисел

--- lab04/3/src/main.c
+++ lab04/3/src/main.c
@@ -1,30 +1,26 @@
 int main() {
 
-	int k=2;
-	int m=1;
-	int n=3; 
+	int k = 2;
+	int m = 1;
+	int n = 3; 
 
 	int temp; 
 
-		if (k<n) {
-			temp=k;
-			k=n;
-			n=temp;
+		if (k > n) {
+			temp = k;
+			k = n;
+			n = temp;
 		}
-		if (k<m) {
-			temp=k;
-			k=m;
-			m=temp;
+		if (k > m) {
+			temp = k;
+			k = m;
+			m = temp;
 		}
-		if (n<k) {
-			temp=n;
-			n=k;
-			k=temp;
+		if (m > n) {
+			temp = m;
+			m = n;
+			n = temp;
 		}
-		if (m<n) {
-			temp=m;
-			m=n;
-			n=temp;
-		}
+	
 	return 0;
 }
