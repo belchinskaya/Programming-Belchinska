6680d3bb6c49e6bc719d61e915fcafeaa043eec7
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-18 18:35:04 +0200

    вихначити y(x) на заданих проміжках графіку

--- lab04/5/src/main.c
+++ lab04/5/src/main.c
@@ -4,14 +4,11 @@ int main() {
 	if (X < -1) {
 		y = -1 * X - 1;
 	}
-	else if (X == -1) {
-		y = 0;
+	else if (X >= -1 && X <= 0) {
+		y = X + 1;
 	}
-	else if (X != 0) {
-		y = 1;
-	}
-	else if (X == 1) {
-		y = 0;
+	else if (X > 0 && X <= 1) {
+		y = -1 * X + 1;
 	}
 	else if (X > 1) {
 		y = X - 1;
