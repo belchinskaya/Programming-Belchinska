cf9c5154a2e0285fa26339384baba05995535188
Kate Belchinskaya <k.belchinskaya@gmail.com>
2020-11-21 15:53:36 +0200

    update Makefile

--- lab03/3.6/src/main.c
+++ lab03/3.6/src/main.c
@@ -1,7 +1,10 @@
 #define N1 50
 #define N2 52
 int main() {
-	int n = N2 - N1;
-	int num = ((N1 + N2) / 2 * n) + N2;
+	int sum;//число в сумі 
+	int n = N2 - N1 + 1;
+	int chislitel = (N1 + N2;)
+	float drob = chislitel / 2.0;
+	sum = drob * n;
 	return 0;
 }
