148b0ac71a23c923a03c991dc448feb4357cdab4
belchinskaya <k.belchinskaya@gmail.com>
2020-11-26 00:18:08 +0200

    число у рядок

--- lab07/task3_lab06/src/main.c
+++ lab07/task3_lab06/src/main.c
@@ -1,14 +1,15 @@
-#define NUM 4
-void compareNums(int arrayNums[], int len);
-int copyOne(char numAsString[], int iStart, char numStr[], int size) {
-	for (int t = 0; t < size; t++) {
+#include <stdlib.h>
+#include <time.h>
+int compareNums(int arrayNums[], int len);//попереднє оголошення масиву для порівняння чисел
+int copyOne(char numAsString[], int iStart, char numStr[], int sizeWord) {/*масив переписує потрібні значення у результуючий масив (числівник або розряд) починаючи з першого незаповненого елементу*/
+	for (int t = 0; t < sizeWord; t++) {
 		numAsString[iStart] = numStr[t];
 		iStart++;
 	}
-	return iStart;
+	return iStart;//після будь-якого заповнення елементу наступне заповнення починається з пустого елементу
 }
 
-int copyNum(int num, char numAsString[], int iStart) {
+int copyNum(int num, char numAsString[], int iStart) {/*виходячи з того, що за цифра, виділяємо кількість елементів в масиві для запису назви числа*/
 	char one[3] = {'o','n','e'};
 	char two[3] = {'t','w','o'};
 	char three[5] = {'t','h','r','e','e'};
@@ -51,15 +52,15 @@ int copyNum(int num, char numAsString[], int iStart) {
 }
 
 
-void fillStr(char numAsString[], int num) {
+void fillStr(char numAsString[], int numIn) {//заповнюємо масив назвами числа та розряду
 	short size = 0;
-
+	int num = numIn;
 	char thousand[10] = {' ','t','h','o','u','s','a','n','d',' '};
 	char hundred[9] = {' ','h','u','n','d','r','e','d',' '};
 	char decade[8] = {' ','d','e','c','a','d','e',' '};
 	int cond = 0;
 
-	for (int i = 10000; i > 0; i /= 10) {
+	for (int i = 10000; i > 0; i /= 10) {//оприділяємо розрядність заданого числа
 		if (num > 0) {
 			cond = num % i;
 			if (cond != num) {
@@ -69,22 +70,22 @@ void fillStr(char numAsString[], int num) {
 		}
 	}
 
-	num = NUM;
+	num = numIn;
 	int iStart = 0;
 
 	if (size == 4) {
-		num = NUM / 1000;
-		iStart = copyNum(num, numAsString, iStart);
-		iStart = copyOne(numAsString, iStart, thousand, 10);
+		num = numIn / 1000;
+		iStart = copyNum(num, numAsString, iStart);//число
+		iStart = copyOne(numAsString, iStart, thousand, 10);//разряд
 		size--;
-		num = NUM % 1000;
+		num = numIn % 1000;
 	}
 	if (size == 3) {
 		num = num / 100;
 		iStart = copyNum(num, numAsString, iStart);
 		iStart = copyOne(numAsString, iStart, hundred, 9);
 		size--;
-		num = NUM % 100;
+		num = num % 100;
 	}
 
 	if (size == 2) {
@@ -95,31 +96,38 @@ void fillStr(char numAsString[], int num) {
 	}
 
 	if (size == 1) {
-		num = NUM % 10;
+		num = num % 10;
 		iStart = copyNum(num, numAsString, iStart);
 		numAsString[iStart] = '\0';
 	}
 }
 
 int main() {
-	char arr[50] = {0};
-	fillStr(arr, NUM);
-	int len = rand() % 10 - 5;
+	srand(time(0));
+	int numIn = rand() % 9999 + 1;
+	char arr[50] = {0};/*усі дії у функції будуть проходити з цим масивом*/
+	fillStr(arr, numIn);
+	
+	
+	
+	const int len = 5;//довжинна масиву для порівняння чисел поряд
 	char arrayNums[len];
-	compareNums(arrayNums, len);
+	int numOfCompare = compareNums(arrayNums, len);
+	
 	return 0;
 	
 	
 }
 
-void compareNums(int arrayNums[], int len) {
+int compareNums(int arrayNums[], int len) {//порівняти число з наступним, порахувати кількість пар де наступне число більше минулого
 	int count = 0;
-	len = rand() % 10 - 5;
-	int arrayNumsOut[len];
-	
-	for (int i = 0; i < len; i++) {
-		if (arrayNumsOut[i] < arrayNumsOut[i + 1]) {
+	for (int j = 0; j < len - 1; j++) {//рандомними числами заповнюємо масив
+		arrayNums[j] = rand() % 5 - 2;
+	}
+	for (int i = 0; i < len - 1; i++) {//порівнюємо числа поряд
+		if (arrayNums[i] < arrayNums[i + 1]) {
 			count++;
 		}
 	}
+	return count;//повертаємо результат у функцію main
 }
