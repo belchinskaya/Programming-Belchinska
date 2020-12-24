c64254ee39512845e1cb120c191a7b6508c3a4e9
belchinskaya <k.belchinskaya@gmail.com>
2020-12-24 09:06:36 +0200

    знайти числа у тексті

--- lab13/task4/src/lib.c
+++ lab13/task4/src/lib.c
@@ -1,19 +1,40 @@
+/**
+ * @file lib.c
+ * @brief Файл з реалізацією функцій
+ *
+ * @author Belchynska K.
+ * @date 20-dec-2020
+ * @version 1.0
+ */
 #include "lib.h"
 
-void getNum(char* strIn, char* textOut) {
-    char * pStrIn = strIn;
-    //strOut[50] = {0};
-    for (int i = 0; i < strlen(strIn); i++) {
-        if (isdigit(*(pStrIn + i)) != 0) {
-            //char temp = *(pStrIn + i);
-            strcat(strOut, *(pStrIn + i));
-        } else {
-            strcat(strOut, ",");
-            strcat(strOut, " ");
-            break;
-        }
-    }
-    free(strIn);
-    free(pStrIn);
+int countOfDigits(char *in) {
+	char *text = (char*) malloc ((strlen(in) + 1) * sizeof(char));
+	strcpy(text, in);
+
+	int result = 0;
+	int elCount = 0;
+	
+	while (true) {
+		text = strpbrk(text, "0123456789");
+		if (text != NULL) {
+			result++;
+			elCount = strspn(text, "0123456789");
+			text = text + elCount;
+		} else {
+			break;
+		}
+	}
+		
+
+	return result;
 }
 
+void fillArrayByDigits(int *out, char *in, int count) {
+	for (int i = 0; i < count; i++) {
+		in = strpbrk(in, "0123456789");
+		int temp = strspn(in, "0123456789");
+		*(out + i) = atoi(in);
+		in = in + temp;
+	}
+}
--- lab13/task4/src/main.c
+++ lab13/task4/src/main.c
@@ -1,7 +1,45 @@
+/**
+ * @mainpage
+ * # Індивідуальне завдання
+ * @brief **Знайти** всі числа, що зустрічаються в тексті
+ *
+ * @author Belchynska K.
+ * @date 20-dec-2020
+ * @version 1.0
+ */
+
+/**
+ * @file main.c
+ * @brief Головний файл з викликом функцій,
+ * виділенням пам'яті для динамічних масивів та заданням вхідного тексту.
+ *
+ *
+ * @author Belchynska K.
+ * @date 20-dec-2020
+ * @version 1.0
+ */
 #include "lib.h"
+
+/**
+ * Головна функція.
+ *
+ * Послідовність дій:
+ * - завдання масиву с текстом
+ * - виділення пам'яті для результуючого масиву
+ * - виклик функції для знаходження чисел
+ * - виклик функції для заповнення результуючого масиву
+ * @return 0 при успішному завершенні програми
+ */
+
 int main() {
-    char* textIn = "123, fdk, 34, 45 4";
-    char * textOut = (char*) malloc(strlen(textIn) * sizeof(char));
-    getNum(textIn, textOut);
-    return 0;
+	char text[] = "3when76 you're 123 1 r567ead4y 548 , 9 come &677 get itÎ";
+	char *ptext = text;
+
+	int digitCount = countOfDigits(ptext);
+
+	int *arr = (int*) malloc (digitCount * sizeof(int));
+
+	fillArrayByDigits(arr, ptext, digitCount);
+
+	return 0;
 }
