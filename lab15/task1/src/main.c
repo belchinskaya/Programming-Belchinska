1c858fc5d3de88373a2a5692abf235dfb7f4e604
belchinskaya <k.belchinskaya@gmail.com>
2021-02-16 22:27:26 +0200

    lab15

--- lab15/task1/src/main.c
+++ lab15/task1/src/main.c
@@ -1,4 +1,22 @@
-//
-// Created by kate on 08.02.21.
-//
+#include "lib.h"
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("/Kate/firms.txt", "r");
+
+ /*   long count = fileSize(file);
+    pFirms = (char*) malloc (count * sizeof(char));
+    readFromFile(pFirms, count);
+    writeToConsole(pFirms, count);
+
+    readFromFile(pFirms);
+
+    struct Instrument violin;
+    strcpy(violin-firm, "Stenor");
+
+    fclose(file);
+    */
+    return 0;
+}
+
 
