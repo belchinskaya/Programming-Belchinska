4d397d3a5d3fd958dbd0f73a70ed2eaa5fcf47d8
belchinskaya <k.belchinskaya@gmail.com>
2021-03-15 20:44:03 +0200

    lab18

--- lab18/src/main.c
+++ lab18/src/main.c
@@ -1,13 +1,38 @@
 #include "lib.h"
 
 int main() {
-    char * array1 = malloc(100 * sizeof(char));
-    for (int i = 0; i < 100; i++) {
 
-    }
+
 
     insert("abracadabra", "TEXT2", 4);
     delete_array("abracadabra", 4, 8);
 
+//FILE * file = fopen("/home/kate/Programming-Belchynska/lab15/InstrumentsOut.txt", "r");
+    struct Instrument ** instruments = malloc(INSTRUMENT_COUNT * sizeof(struct Instrument* ));
+    for (int i = 0; i < 200; i++) {
+        *(instruments + i) = malloc(sizeof(struct Instrument));
+    }
+//    struct Bow ** bows = malloc(6 * sizeof(struct Bow ));
+//    for (int i = 0; i < 200; i++) {
+//        *(bows + i) = malloc(sizeof(struct Bow));
+//    }
+    enum Material* material = malloc(sizeof(enum Material));
+    struct Bow* bow1 = malloc(sizeof(struct Bow));
+
+    readFromFile(instruments, bow1, material);
+    sortByYear(instruments);
+    deleteStructElement(instruments, bow1, material);
+    //printInstrument(instruments, bows);
+    printTheOldestInstrument(instruments, bow1, material);
+
+    writeInFile(instruments, bow1, material);
+    writeToBinaryFile(instruments, bow1, material);
+    readFromBinary( instruments, bow1, material);
+    copyStructElement(instruments, bow1, material);
+
+
+    free(material);
+    free(instruments);
+    free(bow1);
     return 0;
 }
--- lab18/test/test.c
+++ lab18/test/test.c
@@ -7,29 +7,44 @@ bool testInsert() {
     char arr2[] = "Text2";
     char expected[]= "abrText2acadabra";
     char actual[17];
-    size_t pos = 3;
 
 
     strcpy(actual, insert(arr1, arr2, 3));
 
     if (strcmp(actual, expected) == 0){
         result = true;
-        printf("Expected array: %s\n", expected);
-        printf("Actual array: %s\n", actual);
+        printf("\nExpected array for function testInsert: %s\n", expected);
+        printf("Actual array for function testInsert: %s\n\n", actual);
     } else {
         result = false;
-        printf("Expected array: %s\n", expected);
-        printf("Actual array: %s\n", actual);
+        printf("Expected array for function testInsert: %s\n", expected);
+        printf("Actual array for function testInsert: %s\n\n", actual);
     }
     return result;
 }
 
 bool test_delete_array(){
-    
+    bool result = true;
+    char arr1[] = "testForDelete";
+    char expected[]= "testDelete";
+    char actual[11];
+    strcpy(actual, delete_array(arr1, 4, 7));
+    if (strcmp(actual, expected) == 0){
+        result = true;
+        printf("Expected array for function delete_array: %s\n", expected);
+        printf("Actual array for function delete_array: %s\n\n", actual);
+    } else {
+        result = false;
+        printf("Expected array for function delete_array: %s\n", expected);
+        printf("Actual array for function delete_array: %s\n\n", actual);
+    }
+    return result;
+
 }
 int main(){
     bool result = true;
     result &= testInsert();
+    result &= test_delete_array();
     if (result){
         printf("All test succeed!\n");
     } else {
