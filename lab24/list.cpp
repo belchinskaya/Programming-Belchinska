90e056a55e8bbf1d8c0bcc39fa056aaec4387d79
belchinskaya <k.belchinskaya@gmail.com>
2021-04-21 11:43:51 +0300

    lab23

--- lab24/list.cpp
+++ lab24/list.cpp
@@ -1,4 +1,36 @@
-//
-// Created by kate on 19.04.21.
-//
+#include "data.h"
+#include <cstddef>
+#include <strings.h>
+
+#pragma once
+class List{
+    Instrument** instrument;
+    size_t count;
+public:List():  count(0){
+
+    }
+    List(const List& copy):  count(copy.count){
+        instrument = new Instrument*[count];
+        for (int i = 0; i < count; ++i) {
+            instrument[i]= new Instrument(*copy.instrument[i]);
+        }
+    }
+
+    Instrument& getInstrument(size_t index);
+    void addInstrument(Instrument& instrument);
+    void removeInstrument(size_t pos);
+    void showInstrument();
+    void findTheOldestInstr(char name[15]);
+
+//add distruct
+    virtual ~List(){
+        for (int i = 0; i < count; ++i) {
+            delete instrument[i];
+        }
+        delete instrument;
+    }
+};
+
+
+
 
